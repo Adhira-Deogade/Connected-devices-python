63ba3eb9273c372bac54a7ba50504c1defa36e74
Adhira <deogade.a@husky.neu.edu>
2019-04-21 17:54:45 -0400

    Project demo files

--- apps/labs/module08/RpiPublisherFinal.py
+++ apps/labs/module08/RpiPublisherFinal.py
@@ -4,11 +4,13 @@ Created on 18-Apr-2019
 @author: Adhira
 '''
 
-
 '''
 Sends data to Ubidots using MQTT over TLS
 
 Example provided by Jose Garcia @Ubidots Developer
+'''
+'''
+Change address to Rpi module directory
 '''
 import sys
 sys.path.insert(0,'/home/pi/workspace/iot-device/connected-devices-python/apps')
@@ -19,10 +21,18 @@ import json
 import ssl
 from labs.module02 import SmtpClientConnector
 from sense_hat import SenseHat
+'''
+Create pygame object and inmort song for starting music as alarm
+'''
 import pygame
 pygame.mixer.init()
 pygame.mixer.music.load("song.mp3")
+
+'''
+Import sensor data object to send sensor data information to email
+'''
 from labs.common import SensorData
+
 '''
 global variables
 '''
@@ -30,18 +40,25 @@ global variables
 connected = False  # Stores the connection status
 BROKER_ENDPOINT = "things.ubidots.com"
 TLS_PORT = 8883  # Secure port
-MQTT_USERNAME = "A1E-oIBRNfbQ3Soe81cuUBmR10PozvY5co"  # Put here your Ubidots TOKEN
-MQTT_PASSWORD = ""  # Leave this in blank
+MQTT_USERNAME = "A1E-oIBRNfbQ3Soe81cuUBmR10PozvY5co"  # Ubidots TOKEN
+MQTT_PASSWORD = ""  
 TOPIC = "/v1.6/devices/"
 DEVICE_LABEL = "HomeAutomation"
-TLS_CERT_PATH = "/home/pi/workspace/iot-device/connected-devices-python/apps/labs/module08/industrial.pem"  # Put here the path of your TLS cert
+TLS_CERT_PATH = "/home/pi/workspace/iot-device/connected-devices-python/apps/labs/module08/industrial.pem" # Path of your TLS cert
 sen = SmtpClientConnector.SmtpClientConnector()
 red = (255,0,0)
 sensor = SensorData.SensorData()
+
 '''
 Functions to process incoming and outgoing streaming
 '''
-
+'''
+    @summary: Handling connection callback
+    @param client: Specify the client to be connected
+    @param userdata: Specifying the user data/topic
+    @param flag, rc: Flag and return code 
+    @return: Connection flag as True/False  
+'''
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
 
@@ -51,11 +68,22 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("[INFO] Error, connection failed")
 
-
+'''
+    @summary: Handle publishing callback
+    @param client: Specify the client to be connected
+    @param userdata: Specifying the user data/topic 
+    @return: Published as True/False  
+'''
 def on_publish(client, userdata, result):
     print("Published!")
 
-
+'''
+    @summary: Connecting to broker with certificate
+    @param mqtt_client: Specify the client to be connected
+    @param mqtt_username, mqtt_password Specifying user details
+    @param broker_endpoint, port: specifying connection details
+    @return: Connection flag as True/False  
+'''
 def connect(mqtt_client, mqtt_username, mqtt_password, broker_endpoint, port):
     global connected
 
@@ -84,7 +112,12 @@ def connect(mqtt_client, mqtt_username, mqtt_password, broker_endpoint, port):
 
     return True
 
-
+'''
+    @summary: Publishing to broker
+    @param mqtt_client: Specify the client to be connected
+    @param topic: Specifying topic to be published
+    @param payload: Specifying the data to be sent under the topic 
+'''
 def publish(mqtt_client, topic, payload):
 
     try:
@@ -93,7 +126,12 @@ def publish(mqtt_client, topic, payload):
     except Exception as e:
         print("[ERROR] Could not publish data, error: {}".format(e))
 
-
+'''
+    @summary: Main function to obtain data from sensor,create condition and send email message by calling SMTP client connector
+              Creating payload and sending sensor data in json format, and publish to broker. Perform this continuously in a loop,
+              Obtain fresh data by clearing the sensehat.
+    @return: Success flag as True/False  
+'''
 def main():
     
     topic = "{}{}".format(TOPIC, DEVICE_LABEL)
@@ -101,35 +139,42 @@ def main():
     mqtt_client = mqttClient.Client()
     
     while True:
+        ''' Create sense hat object'''
         sense = SenseHat()
         acceleration = sense.get_accelerometer_raw()
         tempData = sense.get_temperature()
         humidData = sense.get_humidity()
         pressureData = sense.get_pressure()
         
+        ''' Obtain acceleration information'''
         x1 = acceleration['x']
         y1 = acceleration['y']
         z1 = acceleration['z']
-     
+        
+        ''' Obtain absolute value to  indicate motion'''
         x = abs(x1)
         y = abs(y1)
         z = abs(z1)
+        
+        ''' Send data to sensor object in order to send to email'''
         sensor.addValue(tempData, pressureData, humidData)
         print("Sending sensor data...")
         sen.publishMessage("Updated sensor information", sensor)
         print("Temperature - ", tempData, "Pressure - ", pressureData, "Humidity - ", humidData)
+        
+        ''' Condition to  send intrusion detection information'''
         if x > 1 or y > 1 or z > 1:
             sense.show_letter("!", red)
             print("Sending notification...")
             sen.publishMessage("Alert", "Intruder Detected")
             print("Message sent.!")
+            
+        ''' Turn the speaker ON and start playing music'''
         pygame.mixer.music.play()
         time.sleep(10)
         pygame.mixer.music.pause()
-    #while pygame.mixer.music.get_busy() == True:
-    #continue
-        #else:
-            #break
+    
+        ''' Create payload by dumping sensor data information in json object and publish'''
         payload = json.dumps({"Temperature": tempData,
                               "Pressure":pressureData,
                               "Humidity":humidData,
@@ -139,6 +184,8 @@ def main():
             return False
         publish(mqtt_client, topic, payload)
         time.sleep(5)
+        
+    ''' Clear the information from sense hat'''
     sense.clear()
     return True
     
--- apps/labs/module08/RpiSubscriber.py
+++ apps/labs/module08/RpiSubscriber.py
@@ -3,13 +3,18 @@ Created on 20-Apr-2019
 
 @author: Adhira
 '''
-
+'''
+Change address to Rpi module directory
+'''
 import sys,os
 sys.path.insert(0,'/home/pi/workspace/iot-device/connected-devices-python/apps')
 import paho.mqtt.client as mqtt
 from sense_hat import SenseHat
+
+''' Create sense hat object '''
 sense = SenseHat()
 
+''' Variables '''
 BROKER_ENDPOINT = "things.ubidots.com"
 PORT = 1883
 MQTT_USERNAME = "A1E-oIBRNfbQ3Soe81cuUBmR10PozvY5co"  # Put here your TOKEN
@@ -20,6 +25,15 @@ VARIABLE_LABEL1 = "averagetemperature"
 VARIABLE_LABEL2 = "temperature"
 red = (255,0,0)
 
+'''
+Functions to process incoming and outgoing streaming
+'''
+'''
+    @summary: Handling connection callback
+    @param mqttc: Specify the client to be connected
+    @param obj: Specifying the user data/topic
+    @param flags, rc: Flag and return code 
+'''
 def on_connect(mqttc, obj, flags, rc):
     print("[INFO] Connected!")
     topic1 = "{}/{}/{}/lv".format(TOPIC, DEVICE_LABEL, VARIABLE_LABEL1)
@@ -28,6 +42,12 @@ def on_connect(mqttc, obj, flags, rc):
     print(topic2)
     mqttc.subscribe((topic1,0),(topic2,0))
 
+'''
+    @summary: Obtain data from cloud as message, call sensehat object and display the information
+    @param mqttc: Specify the client to be connected
+    @param obj: Specifying the user data/topic
+    @param msg: Define the message 
+'''
 def on_message(mqttc, obj, msg):
     myMsg = float(msg.payload)
     print("[INFO] value received: {}".format(myMsg))
@@ -37,18 +57,38 @@ def on_message(mqttc, obj, msg):
         sense.show_message( "Medium", text_colour=red)
     sense.clear()
 
-
+'''
+    @summary: Handling publishing callback
+    @param mqttc: Specify the client to be connected
+    @param obj: Specifying the user data/topic
+    @param mid: Specifying message id 
+'''
 def on_publish(mqttc, obj, mid):
     print("[INFO] Published!")
 
-
+'''
+    @summary: Handling subscription callback
+    @param mqttc: Specify the client to be connected
+    @param obj: Specifying the user data/topic
+    @param granted_qos: specifying quality of service 
+'''
 def on_subscribe(mqttc, obj, mid, granted_qos):
     print("[INFO] Subscribed!")
 
-
+'''
+    @summary: Handling log callback
+    @param mqttc: Specify the client to be connected
+    @param obj: Specifying the user data/topic
+    @param String: Specifying the string to be displayed in log message 
+'''
 def on_log(mqttc, obj, level, string):
     print("[INFO] Log info: {}".format(string))
 
+'''
+    @summary: Main function to connect to broker, publish and subscribe to specified topics.
+              Perform this continuously in a loop.
+              Subscribing and obtaining data from 2 topics
+'''
 def main():
     # Setup MQTT client
     mqttc = mqtt.Client()
@@ -58,19 +98,13 @@ def main():
     mqttc.on_publish = on_publish
     mqttc.on_subscribe = on_subscribe
 
-    # Uncomment to enable debug messages
-    # mqttc.on_log = on_log
-
     mqttc.connect(BROKER_ENDPOINT, PORT, 60)
     topic1 = "{}/{}/{}/lv".format(TOPIC, DEVICE_LABEL, VARIABLE_LABEL1)
     print(topic1)
     topic2 = "{}/{}/{}/lv".format(TOPIC, DEVICE_LABEL, VARIABLE_LABEL2)
     print(topic2)
 
-    #mqttc.subscribe([topic1,topic2], [0,0])
     mqttc.subscribe((topic1,0),(topic2,0))
-
-
     mqttc.loop_forever()
 
 if __name__ == '__main__':
